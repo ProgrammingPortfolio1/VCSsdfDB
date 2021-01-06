dc01b94326864df80f50caae249e9b6efd5fd036
ProgrammingPortfolio1 <70398262+ProgrammingPortfolio1@users.noreply.github.com>
2021-01-07 00:46:01 +0200

    Initial commit

---
+++ VCSsdfDB/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace VCSsdfDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String query = textBox1.Text;
+            String connStr = "Data Source=library.sdf";
+            SqlCeConnection conn = new SqlCeConnection(connStr);
+            SqlCeCommand cmd = new SqlCeCommand(query, conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Command Executed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+        }
+    }
+}
